a97155ab7cd9281016e06c62379f7c5809e386cc
Satyam Sharma <55931484+codedestructed007@users.noreply.github.com>
2023-10-11 16:39:35 +0530

    added train button

---
+++ model_training.py
@@ -0,0 +1,12 @@
+import sys
+from src.logger import logger
+from src.pipeline.train_pipeline import  TrainPipeline
+from src.exception import CustomException
+
+
+try:
+    trainer = TrainPipeline()
+    trainer.initialize_training_pipeline()
+    logger.info('Model training is successful')
+except Exception as e:
+    raise CustomException(e,sys)
--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -1,9 +1,9 @@
+
 import sys
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
 from src.components.data_ingestion import  DataIngestion
 from src.exception import  CustomException
-
 class TrainPipeline:
     def __init__(self):
         pass
@@ -15,7 +15,7 @@ class TrainPipeline:
             transformation_object = DataTransformation()
             transformation_object.data_transformation('artifacts/train.csv','artifacts/train.csv')
             trainer = ModelTrainer()
-            _,_, trainer.initiate_model_training('artifacts/Transformed_datasets/train_df.csv','artifacts/Transformed_datasets/test_df.csv')
+            _,_,=trainer.initiate_model_training('artifacts/Transformed_datasets/train_df.csv','artifacts/Transformed_datasets/test_df.csv')
 
 
         except Exception as e:
